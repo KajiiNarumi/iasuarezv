5bae76370965438a10a82c78efa164d883180b28
I. Antoine Suárez V. <kajiikana69@gmail.com>
2025-08-10 23:33:43 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,47 @@ $(document).ready(function() {
   // add lightbox class to all image links
   $("a[href$='.jpg'], a[href$='.png'], a[href$='.gif']").attr("data-lity", "");
 });
+
+<script>
+(function() {
+  const html = document.documentElement;
+  const minScale = -2;
+  const maxScale = 3;
+  const baseSize = 16; // px
+  let scale = Number(localStorage.getItem('fontScale')) || 0;
+
+  function applyFontSize() {
+    html.style.fontSize = (baseSize + scale * 2) + 'px';
+  }
+
+  function saveScale() {
+    localStorage.setItem('fontScale', scale);
+  }
+
+  // Busca los elementos por ID
+  const inc = document.getElementById('font-increase');
+  const dec = document.getElementById('font-decrease');
+
+  if (inc && dec) {
+    inc.addEventListener('click', function(e) {
+      e.preventDefault();
+      if (scale < maxScale) {
+        scale++;
+        applyFontSize();
+        saveScale();
+      }
+    });
+    dec.addEventListener('click', function(e) {
+      e.preventDefault();
+      if (scale > minScale) {
+        scale--;
+        applyFontSize();
+        saveScale();
+      }
+    });
+
+    // Aplica la fuente al cargar la página
+    applyFontSize();
+  }
+})();
+</script>
